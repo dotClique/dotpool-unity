6e66f0c21080a143450c3b9228c4d8ce00807f90
spillesen@gmail.com <spillesen@gmail.com>
2023-02-15 21:23:05 +0100

    Changeset: 34 Branch comment: [] added a score system for billiards

---
+++ Assets/pockedetector.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class pockedetector : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            GameManager gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+            // Add the score to the player's score
+            gameManager.AddScore(1);
+        }
+    }
+}
